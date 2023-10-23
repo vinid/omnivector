a90e78bc1bc821f4d82ff22f4efc33f8e8ce87e2
vinid <chiccobia@gmail.com>
2023-10-23 15:50:07 -0700

    remove pgvector and added version

--- omnivector/__init__.py
+++ omnivector/__init__.py
@@ -1,4 +1,5 @@
-from omnivector.pgvector import PGVectorDB
 from omnivector.pinecone import PineconeDB
 from omnivector.lancedb import LanceDB
 from omnivector.weaviate import WeaviateDB
+
+__version__ = "0.1.0"
